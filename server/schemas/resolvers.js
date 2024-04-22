dd0a18cb3ffab49652329676a555712ae8fbf30a
Chris Stevenson <chrislstevenson@gmail.com>
2024-04-22 03:53:33 -0400

    Implement login server-side
    - defined resolver, verified token issue using Apollo server

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -6,6 +6,20 @@ const resolvers = {
     // get all users (not needed in production, include for development)
     users: async () => await User.find({}),
   },
+  Mutation: {
+    // log in and issue a signed token
+    login: async (_, { email, password }) => {
+      const user = await User.findOne({ email });
+      // no user with that email
+      if (!user) throw AuthenticationError;
+      // check password
+      const correctPW = await user.isCorrectPassword(password);
+      if (!correctPW) throw AuthenticationError;
+      // sign the token and return it with the user
+      const token = signToken(user);
+      return { token, user };
+    },
+  },
 };
 
 module.exports = resolvers;
