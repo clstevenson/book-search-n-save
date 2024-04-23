2858cb52f76aaaea345574065a7a00bb5f62c8a5
Chris Stevenson <chrislstevenson@gmail.com>
2024-04-22 20:08:47 -0400

    Fix server for production

--- server/server.js
+++ server/server.js
@@ -20,6 +20,9 @@ app.use(express.json());
 // if we're in production, serve client/dist as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/dist')));
+  app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+  })
 }
 
 // Create a new instance of an Apollo server with the GraphQL schema
